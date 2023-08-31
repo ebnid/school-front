2e4502e893cb582233b5d309f6318c6ee57b97ec
ebnid <code@ebnid.com>
2023-08-31 17:03:13 +0600

    deploy

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -51,7 +51,7 @@
                         {{ __('Manage Account') }}
                     </div>
 
-                    <x-dropdown-link href="{{ route('profile.show') }}">
+                    <x-dropdown-link href="{{ route('profile.profile') }}">
                         {{ __('Profile') }}
                     </x-dropdown-link>
 
