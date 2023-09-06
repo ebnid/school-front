cee9df016082151c3fed865912bec77d99fb1d25
ebnid <code@ebnid.com>
2023-09-06 21:19:08 +0600

    deploy

--- resources/views/front/pages/information-gallery.blade.php
+++ resources/views/front/pages/information-gallery.blade.php
@@ -1,6 +1,6 @@
 <x-front-layout>
 
-    <section>
+    <section class="mt-5">
         <div class="container mx-auto">
             <div class="grid grid-cols-1 md:grid-cols-3 gap-5">
                 @php 
