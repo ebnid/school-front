6732d5e66ecd5945ccc64410ae9f2c79b91102f3
ebnid <code@ebnid.com>
2023-09-06 21:50:30 +0600

    deploy

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -37,7 +37,7 @@
         </style>
 
     </head>
-    <body x-data="{ isNavigationOpen: false }" class="font-sans antialiased bg-white md:bg-gray-200 line-backgraound-image" style="background-image: url('{{ asset('assets/images/background-body.jpg') }}')">
+    <body x-data="{ isNavigationOpen: false }" class="font-sans antialiased bg-white md:bg-gray-200">
 
         @include('front.partials.header')
 
