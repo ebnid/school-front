f275170c3f9cdd4a8dd1db87d2341491a74f9d31
ebnid <code@ebnid.com>
2023-09-06 20:29:37 +0600

    deploy

--- resources/views/front/pages/notice-details.blade.php
+++ resources/views/front/pages/notice-details.blade.php
@@ -1,11 +1,13 @@
 <x-front-layout>
     <section class="mt-5">
         <div class="container mx-auto">
-            <div class="bg-white md:p-7 font-bangla">
-                <h1 class="text-4xl font-extrabold text-center">নোটিশ</h1>
-                <h1 class="text-3xl mt-7 text-blue-600 font-extrabold text-center">{{ $notice->name ?? '' }}</h1>
-                <p class="mt-5">{{ $notice->created_at->format('d M Y h:i A') }}</p>
-                <p class="mt-4 text-lg">{!! $notice->content ?? '' !!}</p>
+            <div class="bg-white md:p-7 font-bangla py-16">
+               <div class="max-w-3xl mx-auto">
+                    <h1 class="text-4xl font-extrabold text-center">নোটিশ</h1>
+                    <h1 class="text-3xl mt-7 text-blue-600 font-extrabold text-center">{{ $notice->name ?? '' }}</h1>
+                    <p class="mt-5">{{ $notice->created_at->format('d M Y h:i A') }}</p>
+                    <p class="mt-4 text-lg">{!! $notice->content ?? '' !!}</p>
+               </div>
             </div>
         </div>
     </section>
