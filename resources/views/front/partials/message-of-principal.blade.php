1b2a2d8a03d4f9e5ccf83700830b8f502dc637a3
ebnid <code@ebnid.com>
2023-09-06 21:24:41 +0600

    deploy

--- resources/views/front/partials/message-of-principal.blade.php
+++ resources/views/front/partials/message-of-principal.blade.php
@@ -1,11 +1,11 @@
-<div class="rounded-md md:p-7 bg-white">
+<div class="rounded-md md:p-10 bg-white">
     <h1 class="mb-5 md:mb-10 text-2xl md:text-3xl font-extrabold text-gray-900 text-center"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Message of Principal</span></h1>
     <div class="flex flex-col items-center justify-center">
         <img class="block w-1/3 aspect-square rounded-full object-cover" src="{{ asset('assets/uchakhila-school-images/principal-muhammad-abdul-halim.jpg') }}" alt="" />
         <span class="text-xs font-bold text-gray-400 mt-3 tracking-wider">Mohammad Abdul Halim</span>
     </div>
 
-    <div class="mt-5 text-md space-y-5">
+    <div class="mt-5 text-lg space-y-5">
         <p class="text-justify font-normal text-gray-500 dark:text-gray-400">According to the demand of nation country and to speared modern knowledge of Science & Technology. Since 1932 the traditional ancient institute named Uchakhila Higher Secondary School has been established eliminate education to those who are deprived of modern facilities living at the Ishwarganj upazila under the district of Mymensingh. That is why combined the sections of School, College & Vocational have been initiated in this area since establishment.</p>
         <p class="text-justify font-normal text-gray-500 dark:text-gray-400">We are remembering those who help with their merit, industry, economy etc from their respective places. At present the standard stage of education of this institutions is enhancing day by day and ensuring the standard education management as well as nurturing merit in spite of unlimited obstacles here is no any lack of sincerity and morality to build up merit.</p>
         <p class="text-justify font-normal text-gray-500 dark:text-gray-400">The main purpose of this institute to explore creativity and hidden talent the student with co-curricular activities has been created. Under the strong administrative and in exchange of endless industry of teachers, systematical education management has been ensured here. As a result, the students board exam at every stages have been toped with GPA 5 more than expectation and 15/16 scholarships this upazila.</p>
