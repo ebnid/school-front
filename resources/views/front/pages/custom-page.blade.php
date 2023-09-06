5e762912f3cf447dab2611bacad3f8b1806191f0
ebnid <code@ebnid.com>
2023-09-06 20:31:29 +0600

    deploy

--- resources/views/front/pages/custom-page.blade.php
+++ resources/views/front/pages/custom-page.blade.php
@@ -1,6 +1,6 @@
 <x-front-layout>
-    <div class="{{ $page->lang === 'bangla' ? 'font-bangla' : '' }} container mx-auto rounded-md md:p-10 bg-white mt-5 py-16">
-        <div class="max-w-3xl mx-auto">
+    <div class="{{ $page->lang === 'bangla' ? 'font-bangla' : '' }} container mx-auto rounded-md md:p-10 bg-white mt-5">
+        <div class="max-w-3xl mx-auto py-16">
             <h1 class="text-center mb-6 text-2xl md:text-4xl text-transparent font-extrabold md:text-3xl bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 leading-none tracking-tight">{{ $page->name }} <span class="text-blue-600 dark:text-blue-500"></span></h1>
 
             <div class="space-y-5 text-justify">
