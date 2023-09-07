7347b27de4997614bd95ab5334893c671e553cb5
ebnid <code@ebnid.com>
2023-09-07 14:41:02 +0600

    deploy

--- resources/views/front/partials/banner.blade.php
+++ resources/views/front/partials/banner.blade.php
@@ -3,8 +3,10 @@
         <div class="absolute w-full h-full inset-0 bg-gradient-to-r bg-gradient-to-r from-sky-400 via-emerald-600 to-transparent md:rounded-md" to-transparent md:rounded-xl">
             <div class="flex p-10">
                 <div class="">
-                    <h1 class="mb-4 text-2xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">উচাখিলা উচ্চ বিদ্যালয় ও কলেজে</span></h1>
-                    <h1 class="mb-3 text-lg font-extrabold text-gray-900 dark:text-white"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">ইশ্বরগঞ্জ, ময়মনসিংহ।</span></h1>
+                    <!-- <h1 class="mb-4 text-2xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">উচাখিলা উচ্চ বিদ্যালয় ও কলেজে</span></h1> -->
+                    <h1 class="mb-4 text-2xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-white">উচাখিলা উচ্চ বিদ্যালয় ও কলেজে</span></h1>
+                    <!-- <h1 class="mb-3 text-lg font-extrabold text-gray-900 dark:text-white"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">ইশ্বরগঞ্জ, ময়মনসিংহ।</span></h1> -->
+                    <h1 class="mb-3 text-lg font-extrabold"><span class="text-white">ইশ্বরগঞ্জ, ময়মনসিংহ।</span></h1>
                     <div class="max-w-2xl">
                         @include('front.partials.announcebar')
                     </div>
