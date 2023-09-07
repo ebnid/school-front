597bda09c2750bbc660d91dcc53fa7be13a441d9
ebnid <code@ebnid.com>
2023-09-07 14:36:18 +0600

    deploy

--- resources/views/front/partials/banner.blade.php
+++ resources/views/front/partials/banner.blade.php
@@ -1,6 +1,6 @@
 <section class="mt-3 font-bangla">
     <div class="relative container mx-auto md:rounded-xl h-48" style="background-image: url('https://www.bssnews.net/assets/news_photos/2023/07/28/image-138809-1690531929.jpg')">
-        <div class="absolute w-full h-full inset-0 bg-gradient-to-r from-white via-white to-transparent md:rounded-xl">
+        <div class="absolute w-full h-full inset-0 bg-gradient-to-r from-gray-200 via-gray-100 to-transparent md:rounded-xl">
             <div class="flex p-10">
                 <div class="">
                     <h1 class="mb-4 text-2xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">উচাখিলা উচ্চ বিদ্যালয় ও কলেজে</span></h1>
