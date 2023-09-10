20151649e29dfa0d50054e9c343842c41ac40a94
ebnid <code@ebnid.com>
2023-09-10 21:29:57 +0600

    deploy

--- resources/views/front/partials/message-of-principal.blade.php
+++ resources/views/front/partials/message-of-principal.blade.php
@@ -32,20 +32,25 @@
             <img src="{{ asset('assets/uchakhila-school-images/principal-muhammad-abdul-halim.jpg') }}" alt="">
           </div>
           <div class="flex flex-col items-center text-center justify-center">
-            <h2 class="font-medium title-font mt-4 text-gray-700 text-lg">Mohammad Abdul Halim</h2>
-            <div class="w-12 h-1 bg-black rounded mt-2 mb-4"></div>
-            <p class="text-base">Principal</p>
-            <h2 class="font-medium title-font mt-1 text-gray-700 text-sm">Uchakhila Higher Secondary School</h2>
-            <h2 class="font-medium  mt-1 text-gray-600 text-xs">Ishwargonj, Mymensingh.</h2>
+            <h2 class="font-medium title-font mt-4 text-sky-900 text-lg">Mohammad Abdul Halim</h2>
+            <div class="w-12 h-1 bg-sky-900 rounded mt-2 mb-4"></div>
+            <p class="text-base text-sky-900">Principal</p>
+            <h2 class="font-medium title-font mt-1 text-sky-900 text-sm">Uchakhila Higher Secondary School</h2>
+            <h2 class="font-medium  mt-1 text-sky-900 text-xs">Ishwargonj, Mymensingh.</h2>
 
           </div>
         </div>
-        <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-          <p class="leading-relaxed text-lg mb-4"></p>
+        <div class="text-sky-900 sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
             <p class="leading-relaxed text-lg mb-4">According to the demand of nation country and to speared modern knowledge of Science & Technology. Since 1932 the traditional ancient institute named Uchakhila Higher Secondary School has been established eliminate education to those who are deprived of modern facilities living at the Ishwarganj upazila under the district of Mymensingh. That is why combined the sections of School, College & Vocational have been initiated in this area since establishment.</p>
             <p class="leading-relaxed text-lg mb-4">We are remembering those who help with their merit, industry, economy etc from their respective places. At present the standard stage of education of this institutions is enhancing day by day and ensuring the standard education management as well as nurturing merit in spite of unlimited obstacles here is no any lack of sincerity and morality to build up merit.</p>
-            <p class="leading-relaxed text-lg mb-4">The main purpose of this institute to explore creativity and hidden talent the student with co-curricular activities has been created. Under the strong administrative and in exchange of endless industry of teachers, systematical education management has been ensured here. As a result, the students board exam at every stages have been toped with GPA 5 more than expectation and 15/16 scholarships this upazila.</p>
-            <p class="leading-relaxed text-lg mb-4">The students have been achieving great success in all types of co-curricular activities & sports. Confidently I believed that this institutions will go forward coming days with fame and spreading a lot of reputation, after all this institutions will be succeed in all sectors.</p>
+            <a href="http://web.uchakhilahss.edu.bd/principal-message-details" class="hover:scale-105 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-sky-900 focus:ring-4 focus:ring-blue-300">
+                More
+                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+            <!-- <p class="leading-relaxed text-lg mb-4">The main purpose of this institute to explore creativity and hidden talent the student with co-curricular activities has been created. Under the strong administrative and in exchange of endless industry of teachers, systematical education management has been ensured here. As a result, the students board exam at every stages have been toped with GPA 5 more than expectation and 15/16 scholarships this upazila.</p> -->
+            <!-- <p class="leading-relaxed text-lg mb-4">The students have been achieving great success in all types of co-curricular activities & sports. Confidently I believed that this institutions will go forward coming days with fame and spreading a lot of reputation, after all this institutions will be succeed in all sectors.</p> -->
         </div>
       </div>
     </div>
