d40ba5514b46816d5ae7f18d89ef4b31e38c896b
ebnid <code@ebnid.com>
2023-09-10 18:11:40 +0600

    deploy

---
+++ resources/views/admin/pages/profile/delete.blade.php
@@ -0,0 +1,13 @@
+<x-master-layout title="Security">
+
+    <div class="bg-white rounded-sm">
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.delete-user-form')
+                </div>
+            @endif
+        </div>
+    </div>
+
+</x-master-layout>
---
+++ resources/views/admin/pages/profile/security.blade.php
@@ -0,0 +1,29 @@
+<x-master-layout title="Security">
+
+    <div class="bg-white rounded-sm">
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.update-password-form')
+                </div>
+
+                <x-section-border />
+            @endif
+
+            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.two-factor-authentication-form')
+                </div>
+
+                <x-section-border />
+            @endif
+
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.logout-other-browser-sessions-form')
+            </div>
+
+        </div>
+    </div>
+
+</x-master-layout>
