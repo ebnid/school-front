1097559b18f2cc97727a37d2dbd72d03d8a5c893
ebnid <code@ebnid.com>
2023-09-17 15:52:57 +0600

    deploy

--- resources/views/admin/pages/custom-pages/create.blade.php
+++ resources/views/admin/pages/custom-pages/create.blade.php
@@ -1,10 +1,10 @@
 <x-master-layout title="Create Pages">
-    <div class="grid grid-cols-1 md:grid-cols-6 md:gap-5">
-        <div class="col-span-4">
+    <div class="grid grid-cols-1 md:gap-5">
+        <div>
             <livewire:create-edit-page />
         </div>
 
-        <div class="col-span-2">
+        <div>
             <livewire:page-list />
         </div>
     </div>
--- resources/views/admin/pages/notices/create.blade.php
+++ resources/views/admin/pages/notices/create.blade.php
@@ -1,10 +1,10 @@
 <x-master-layout title="Create Notices">
-    <div class="grid grid-cols-1 md:grid-cols-6 md:gap-5">
-        <div class="col-span-3">
+    <div class="grid grid-cols-1 md:gap-5">
+        <div>
             <livewire:create-edit-notice />
         </div>
 
-        <div class="col-span-3">
+        <div>
             <livewire:notice-list />
         </div>
     </div>
