ebf1ce63a8aaef972576ae1bae084963e329dcca
ebnid <code@ebnid.com>
2023-09-20 23:08:41 +0600

    deploy

--- resources/views/front/partials/monitoring-board-caurosel.blade.php
+++ resources/views/front/partials/monitoring-board-caurosel.blade.php
@@ -19,17 +19,9 @@
                 @endforeach
 
             </div>
-                <button class="swiper-button-prev absolute w-10 h-10 rgiht-10 top-1/2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                    </svg>
-                </button>
+                <button class="swiper-button-prev absolute w-10 h-10 right-10 top-1/2"></button>
 
-                <button class="swiper-button-next absolute w-10 h-10 left-10 top-1/2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                    </svg>
-                </button>
+                <button class="swiper-button-next absolute w-10 h-10 left-10 top-1/2"></button>
             <div class="swiper-pagination"></div>
         </div>
     </div>
