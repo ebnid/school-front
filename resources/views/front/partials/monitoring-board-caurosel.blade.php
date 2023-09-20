19f8dd82b8ba4a611982b35e6a88abec5a716645
ebnid <code@ebnid.com>
2023-09-20 22:52:57 +0600

    deploy

--- resources/views/front/partials/monitoring-board-caurosel.blade.php
+++ resources/views/front/partials/monitoring-board-caurosel.blade.php
@@ -15,20 +15,20 @@
                 @foreach($images as $image)
                     <div class="swiper-slide">
                         <a><img src="{{ $image->imageUrl() }}" class="cursor-pointer w-full max-h-screen object-contain" alt="image" /></a>
-
-                        <button class="caurosel-prev-btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                            </svg>
-                        </button>
-
-                        <button class="caurosel-next-btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                            </svg>
-                        </button>
                     </div>
                 @endforeach
+
+                <button class="caurosel-prev-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                    </svg>
+                </button>
+
+                <button class="caurosel-next-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                    </svg>
+                </button>
 
             </div>
             <div class="swiper-pagination"></div>
