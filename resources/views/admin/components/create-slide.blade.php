2d1ab400faeca396692151979ab2d544407a4695
ebnid <code@ebnid.com>
2023-09-20 19:34:50 +0600

    deploy

--- resources/views/admin/components/create-slide.blade.php
+++ resources/views/admin/components/create-slide.blade.php
@@ -18,7 +18,7 @@
 
         <div>
             <label for="details" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Details</label>
-            <textarea wire:model.debounce="details" id="details" rows="3" class="block p-2.5 w-full text-md text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+            <textarea wire:model.debounce="details" id="details" rows="3" class="block p-2.5 w-full text-md text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="details..."></textarea>
         </div>
 
         <div>
--- resources/views/front/partials/photo-gallery.blade.php
+++ resources/views/front/partials/photo-gallery.blade.php
@@ -2,7 +2,7 @@
 
     $home_gallery = \App\Models\Caurosel::published()->where('show_in_page', 'gallery')->first();
 
-    $images = \App\Models\Slide::published()->where('caurosel_id', $home_gallery->id ?? 0)->paginate(9);
+    $images = \App\Models\Slide::published()->where('caurosel_id', $home_gallery->id ?? 0)->paginate(12);
 
 @endphp
 
@@ -10,19 +10,22 @@
 
 <section class="text-gray-600 body-font">
 
-  <div class="container px-5 py-24 mx-auto">
+  <div class="container px-5 py-16 mx-auto">
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5">
       
         @foreach($images as $image)
             <div>
                 <img class="h-auto max-w-full rounded-lg" src="{{ $image->imageUrl() }}" alt="">
+                @if($image->details)
+                    <p class="font-bangla mt-5">{{ $image->details ?? '' }}</p>
+                @eindif
             </div>
         @endforeach
 
     </div>
 
-    <div class="mt-5">
+    <div class="mt-10">
         {{ $images->links() }}
     </div>
 
