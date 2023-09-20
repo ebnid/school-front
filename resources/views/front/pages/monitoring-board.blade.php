0adabc400725a0ca101e5e5c9342a4d8536a5c76
ebnid <code@ebnid.com>
2023-09-20 22:31:38 +0600

    deploy

--- resources/views/front/pages/monitoring-board.blade.php
+++ resources/views/front/pages/monitoring-board.blade.php
@@ -1,4 +1,4 @@
-<x-front-layout :isShowBanner="{{ false }}">
+<x-front-layout isShowBanner="{{ false }}">
 
     <section class="container mx-auto mt-2 md:mt-5">
         @include('front.partials.monitoring-board-caurosel')
