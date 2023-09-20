67ae3b111e9a636b894f494b8a95e6acd05f34db
ebnid <code@ebnid.com>
2023-09-20 22:54:34 +0600

    deploy

--- resources/views/front/partials/monitoring-board-caurosel.blade.php
+++ resources/views/front/partials/monitoring-board-caurosel.blade.php
@@ -10,7 +10,7 @@
 
     <div class="bg-white md:p-5">
         <div class="swiper max-container mx-auto" id="home-caurosel">
-            <div class="swiper-wrapper">
+            <div class="swiper-wrapper relative">
 
                 @foreach($images as $image)
                     <div class="swiper-slide">
@@ -18,13 +18,13 @@
                     </div>
                 @endforeach
 
-                <button class="caurosel-prev-btn">
+                <button class="caurosel-prev-btn absolute rgiht-10 top-1/2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                     </svg>
                 </button>
 
-                <button class="caurosel-next-btn">
+                <button class="caurosel-next-btn absolute left-10 top-1/2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                     </svg>
