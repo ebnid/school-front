8f043f400d28fb0d36bbcbbfe14f6b07adae93d7
ebnid <code@ebnid.com>
2023-09-20 18:01:09 +0600

    deploy

---
+++ app/Http/Livewire/Attendace/SingleTeacherSummery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Attendace;
+
+use Livewire\Component;
+
+class SingleTeacherSummery extends Component
+{
+    public function render()
+    {
+        return view('admin.components.attendace.single-teacher-summery');
+    }
+}
---
+++ app/Http/Livewire/Attendace/Summery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Attendace;
+
+use Livewire\Component;
+
+class Summery extends Component
+{
+    public function render()
+    {
+        return view('admin.components.attendace.summery');
+    }
+}
