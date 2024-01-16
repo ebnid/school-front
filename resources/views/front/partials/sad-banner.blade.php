acd0fa00a9f1119dd04211987d1e6c31c9c7ada9
ebnid <code@ebnid.com>
2024-01-16 13:32:49 +0600

    dep

--- resources/views/front/partials/sad-banner.blade.php
+++ resources/views/front/partials/sad-banner.blade.php
@@ -1,12 +1,9 @@
-<section class="text-gray-600 body-font bg-white font-bangla>
-  <div class="container mx-auto flex px-5 py-6 md:flex-row flex-col items-center">
-    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">আমরা শোকাহত</h1>
-      <p class="mb-8 leading-relaxed">উচাখিলা উচ্চ মাধ্যমিক বিদ্যালয় ও কলেজের ৪র্থ শ্রেনির খন্ডকালীন কর্মচারী রাজন চন্দ্র ভৌমিক এর মৃত্যুতে আমরা গভীর ভাবে শোকাহত।
-        উচাখিলা উচ্চ মাধ্যমিক বিদ্যালয় ও কলেজের শিক্ষক/কর্মচারী বৃন্দ।</p>
+
+<section class="font-bangla bg-white rounded-md grid grid-cols-1 md:grid-cols-5 px-10 py-6 gap-10">
+    <img class="object-cover block" src="{{ asset('assets/images/rojon-chondro-dash.jpg') }}" alt="">
+    <div class="md:col-span-4">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">আমরা শোকাহত</h1>
+        <p class="mb-8 leading-relaxed">উচাখিলা উচ্চ মাধ্যমিক বিদ্যালয় ও কলেজের ৪র্থ শ্রেনির খন্ডকালীন কর্মচারী রাজন চন্দ্র ভৌমিক এর মৃত্যুতে আমরা গভীর ভাবে শোকাহত।
+          উচাখিলা উচ্চ মাধ্যমিক বিদ্যালয় ও কলেজের শিক্ষক/কর্মচারী বৃন্দ।</p>
     </div>
-    <div class="lg:max-w-lg lg:2/6 md:w-1/2 w-5/6">
-      <img class="object-cover object-center rounded" alt="hero" src="{{ asset('assets/images/rojon-chondro-dash.jpg') }}">
-    </div>
-  </div>
 </section>
