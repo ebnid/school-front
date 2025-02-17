39a17f44f3fb940ffbcfaf7980b0cb59061b2b0c
ebnid <125554078+ebnid@users.noreply.github.com>
2025-02-17 16:29:53 +0600

    Update sad-banner.blade.php

--- resources/views/front/partials/sad-banner.blade.php
+++ resources/views/front/partials/sad-banner.blade.php
@@ -1,10 +1,10 @@
 
 <section class="container mx-auto px-5 font-bangla bg-white rounded-md grid grid-cols-1 md:grid-cols-5 px-10 py-6 gap-10">
-    <img class="object-cover block" src="{{ asset('assets/images/rojon-chondro-dash.jpg') }}" alt="">
+    <img class="object-cover block" src="{{ asset('assets/images/habibur-rahman.jpeg') }}" alt="">
     <div class="md:col-span-4">
         <h1 class="title-font sm:text-4xl lg:text-6xl text-3xl mb-4 font-medium text-gray-900">আমরা শোকাহত</h1>
-        <p class="mb-6 leading-relaxed lg:text-2xl">উচাখিলা উচ্চ মাধ্যমিক বিদ্যালয় ও কলেজের ৪র্থ শ্রেনির খন্ডকালীন কর্মচারী রাজন চন্দ্র ভৌমিক এর মৃত্যুতে আমরা গভীর ভাবে শোকাহত।</p>
-        <p class="mb-10 leading-relaxed lg:text-2xl">তার আত্মার শান্তি কামনা করছি সেই সাথে তাঁর পরিবারের প্রতি সমবেদনা জ্ঞাপন করছি।</p>
+        <p class="mb-6 leading-relaxed lg:text-2xl">উচাখিলা উচ্চ মাধ্যমিক বিদ্যালয় ও কলেজের সিনিয়র শিক্ষক জনাব হাবিবুর রহমান (হাবিব স্যার) এর মৃত্যুতে আমরা গভীর ভাবে শোকাহত।</p>
+        <p class="mb-10 leading-relaxed lg:text-2xl">আমরা তাঁর বিদেহী আত্মার মাগফিরাত কামনা করছি সেই সাথে তাঁর পরিবারের প্রতি সমবেদনা জ্ঞাপন করছি।</p>
 
         <p>-উচাখিলা উচ্চ মাধ্যমিক বিদ্যালয় ও কলেজের শিক্ষক/কর্মচারী বৃন্দ। </p>
     </div>
